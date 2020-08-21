e1388c5c414d4f9d879b0a7b86b5f12df14c7dc8
Jaskaran Batra <jskrnbtr@gmail.com>
2020-08-21 14:05:43 +0530

    Create 21_Sort Array By Parity.cpp
    Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
    
    You may return any answer array that satisfies this condition.
    
     
    
    Example 1:
    
    Input: [3,1,2,4]
    Output: [2,4,3,1]
    The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
     
    
    Note:
    
    1 <= A.length <= 5000
    0 <= A[i] <= 5000

---
+++ 21_Sort Array By Parity.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int i=0,j=A.size()-1;
+        while(i<=j){
+            if(A[i]%2==0){
+                i++;
+            }
+            else if(A[j]%2!=0){
+                j--;
+            }
+            else{
+                swap(A[i],A[j]);
+                i++;
+                j--;
+            }
+        }
+        return A;
+    }
+};
